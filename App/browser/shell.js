aca3ea3ca04687c272685d1d6a3b11bb5a1a03a0
Alvaro del Castillo <acs@bitergia.com>
2013-05-23 19:47:04 +0200

    Ok, we have our first views composition. Report converting is not working yet.

--- App/browser/shell.js
+++ App/browser/shell.js
@@ -4,14 +4,8 @@
         router: router,
         activate: function () {
             router.map([
-                { route: '', moduleId: 'samples/hello/index', title: 'Hello World', nav: true },
-                { route: 'view-composition', moduleId: 'samples/viewComposition/index', title: 'View Composition', nav: true },
-                { route: 'modal', moduleId: 'samples/modal/index', title: 'Modal Dialogs', nav: true },
-                { route: 'event-aggregator', moduleId: 'samples/eventAggregator/index', title: 'Events', nav: true },
-                { route: 'widgets', moduleId: 'samples/widgets/index', title: 'Widgets', nav: true },
-                { route: 'master-detail', moduleId: 'samples/masterDetail/index', title: 'Master Detail', nav: true },
-                { route: 'knockout-samples/:name', moduleId: 'samples/knockout/index', title: 'Knockout Samples' },
-                { route: 'knockout-samples', moduleId: 'samples/knockout/index', title: 'Knockout Samples', nav: true }
+                { route: '', moduleId: 'browser/summary/index', title: 'Summary', nav: true },
+                { route: 'summary', moduleId: 'browser/summary/index', title: 'Summary', nav: true }
             ]).buildNavigationModel();
             
             return router.activate();
