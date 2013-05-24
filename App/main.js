f132648311247e878d26677be94c69df2f198138
Alvaro del Castillo <acs@bitergia.com>
2013-05-24 17:50:21 +0200

    Viz are now loaded, yeah! With the afterCompose event!

--- App/main.js
+++ App/main.js
@@ -9,7 +9,7 @@ define(['durandal/app', 'durandal/system', 'durandal/viewLocator'],
     
     system.debug(true);
     
-    app.title = 'Durandal Samples';
+    app.title = 'Metrics Browser';
     app.start().then(function () {
         //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
         //Look for partial views in a 'views' folder in the root.
