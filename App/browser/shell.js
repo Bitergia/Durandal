4533a63210ec16ceb76ff1f5f495c61ae91f01a1
Alvaro del Castillo <acs@bitergia.com>
2013-05-26 12:14:34 +0200

    Added new scm page. We need more testing

--- App/browser/shell.js
+++ App/browser/shell.js
@@ -1,11 +1,11 @@
-﻿define(['durandal/plugins/router'], function (router) {
-    
+﻿define(['durandal/plugins/router'], function (router) {    
     return {
         router: router,
         activate: function () {
             router.map([
                 { route: '', moduleId: 'browser/summary/index', title: 'Summary', nav: true },
-                { route: 'summary', moduleId: 'browser/summary/index', title: 'Summary', nav: true }
+                { route: 'summary', moduleId: 'browser/summary/index', title: 'Summary', nav: true },
+                { route: 'scm', moduleId: 'browser/scm/index', title: 'SourceCode', nav: true }
             ]).buildNavigationModel();
             
             return router.activate();
