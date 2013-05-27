7af8913f6db8d0156879a65dc51eaa9155f49d25
Alvaro del Castillo <acs@bitergia.com>
2013-05-27 08:10:57 +0200

    Ok, now we control when the DOM is ready and can convert in Report

--- App/browser/summary/index.js
+++ App/browser/summary/index.js
@@ -1,3 +1,15 @@
-﻿define(function() {
-    return {};
+﻿define(['durandal/system','durandal/app'], function (system,app) {
+    function activate() {
+        system.log('View Activating', this);
+    }
+
+    function viewAttached() {
+        system.log('View Activated', this);
+        Report.convertGlobal();
+    }
+    
+    return {
+        activate: activate,
+        viewAttached: viewAttached
+    };
 });
